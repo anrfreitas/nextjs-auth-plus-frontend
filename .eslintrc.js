989017f79d6ea2a3882992407c11523daf55db7f
Andre Freitas <andre@sharpcomputing.io>
2023-01-12 22:51:14 -0300

    eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+      "sourceType": "module",
+      tsconfigRootDir: __dirname,
+      project: './tsconfig.json',
+      ecmaVersion: 12,
+  },
+  "settings": {
+    "import/resolver": {
+      "typescript": {}
+    },
+  },
+  "plugins": [
+      "@typescript-eslint/eslint-plugin",
+      "prettier"
+  ],
+  "extends": [
+      "plugin:@typescript-eslint/recommended",
+      "plugin:import/recommended",
+      "plugin:import/typescript",
+      "prettier"
+  ],
+  "root": true,
+  "env": {
+      "node": true,
+      "jest": true
+  },
+  "ignorePatterns": [
+      ".eslintrc.js"
+  ],
+  "rules": {
+      "prettier/prettier": "error",
+      "@typescript-eslint/interface-name-prefix": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": [2, {"ts-ignore": "allow-with-description"}],
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      "@typescript-eslint/ban-types": "off",
+      "import/order": 2
+  }
+};
