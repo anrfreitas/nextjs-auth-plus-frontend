64512bffa3a67219c02b4fb86022b4d7c1f3f9ad
Andre Freitas <andre@sharpcomputing.io>
2023-01-13 00:00:11 -0300

    fixing eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,33 @@
 module.exports = {
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-      "sourceType": "module",
-      tsconfigRootDir: __dirname,
-      project: './tsconfig.json',
-      ecmaVersion: 12,
-  },
-  "settings": {
-    "import/resolver": {
-      "typescript": {}
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json'
     },
-  },
-  "plugins": [
-      "@typescript-eslint/eslint-plugin",
-      "prettier"
-  ],
-  "extends": [
-      "plugin:@typescript-eslint/recommended",
-      "plugin:import/recommended",
-      "plugin:import/typescript",
-      "prettier"
-  ],
-  "root": true,
-  "env": {
-      "node": true,
-      "jest": true
-  },
-  "ignorePatterns": [
-      ".eslintrc.js"
-  ],
-  "rules": {
-      "prettier/prettier": "error",
-      "@typescript-eslint/interface-name-prefix": "off",
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/explicit-module-boundary-types": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/ban-ts-comment": [2, {"ts-ignore": "allow-with-description"}],
-      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-      "@typescript-eslint/ban-types": "off",
-      "import/order": 2
-  }
+    "plugins": [
+        "@typescript-eslint",
+        "@typescript-eslint/eslint-plugin",
+        "prettier"
+    ],
+    "extends": [
+        "next/core-web-vitals",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/typescript",
+        "prettier"
+    ],
+    "rules": {
+        "@typescript-eslint/no-unused-vars": "error",
+        "@typescript-eslint/no-explicit-any": "error",
+        "prettier/prettier": "error",
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/ban-ts-comment": [2, {"ts-ignore": "allow-with-description"}],
+        "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+        "@typescript-eslint/ban-types": "off",
+        "import/order": 2
+    },
+    "ignorePatterns": [
+        ".eslintrc.js"
+    ],
 };
